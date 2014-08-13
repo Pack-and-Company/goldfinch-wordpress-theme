c0c46ea377cf836ea2331f4fb43174f0619f9319
Tom Henderson <tomhenderson@mac.com>
2014-08-13 13:52:56 +1200

    Tidy javascript linting errors.

--- js/nbw-parallax.js
+++ js/nbw-parallax.js
@@ -18,17 +18,15 @@ $(document).ready(function() { //when the document is ready...
 	$("area[rel^='prettyPhoto']").prettyPhoto();
 	$(".gallery:first a[rel^='prettyPhoto']").prettyPhoto({
 		animation_speed:'normal',
-		slideshow:8000, 
-		social_tools:false, 
-		autoplay_slideshow: false, 
-		overlay_gallery:false, 
+		slideshow:8000,
+		social_tools:false,
+		autoplay_slideshow: false,
+		overlay_gallery:false,
 		show_title: true,
 		allow_resize: true,
 		default_width: 320,
 		wmode: 'opaque'
 	});
-
-	
 	
 	//save selectors as variables to increase performance
 	var $window = $(window);
@@ -44,16 +42,15 @@ $(document).ready(function() { //when the document is ready...
 	//$('#intro, #fifth').css('min-height',windowHeight);
 	//apply the class "inview" to a section that is in the viewport
 	$('#intro, #second, #third, #fourth, #fifth').css('min-height',windowHeight).bind('inview', function (event, visible) {
-			if (visible == true) {
+			if (visible === true) {
 			$(this).addClass("inview");
 			} else {
 			$(this).removeClass("inview");
 			}
 		});
 	
-			
 	//function that places the navigation in the center of the window
-	function RepositionNav(){
+	function RepositionNav() {
 		var windowHeight = $window.height(); //get the height of the window
 		//var navHeight = $('#nav').height() / 2;
 		//var windowCenter = (windowHeight / 2); 
@@ -79,7 +76,7 @@ $(document).ready(function() { //when the document is ready...
 		$('#intro .mainsection').css({"padding-top": newPadding}); 
 	}
 	*/
-	function repos_contact(){
+	function repos_contact() {
 		var storyHeight = $('#second .mainsection').height();
 		var newPadding = (windowHeight - storyHeight)/2;
 		$('#second .mainsection').css({"padding-top": newPadding});
@@ -92,7 +89,7 @@ $(document).ready(function() { //when the document is ready...
 	}
 	
 	//function to be called whenever the window is scrolled or resized
-	function Move(){ 
+	function Move() {
 		var pos = $window.scrollTop(); //position of the scrollbar
 		var windowHeight = $window.height(); //get the height of the window
 		//if the first section is in view...
@@ -101,17 +98,17 @@ $(document).ready(function() { //when the document is ready...
 			$firstBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 620, 0.3)});
 			var storyHeight = $('#intro .mainsection').height();
 			var newPadding = (windowHeight - storyHeight)/2;
-			$('#intro .mainsection').css({"padding-top": newPadding}); 			
+			$('#intro .mainsection').css({"padding-top": newPadding});	
 		}
 		
-		if($secondBG.hasClass("inview")){
+		if($secondBG.hasClass("inview")) {
 			var storyHeight = $('#second .mainsection').height();
 			var newPadding = (windowHeight - storyHeight)/2;
 			$('#second .mainsection').css({"padding-top": newPadding});
 		}
 
 		//if the fourth section is in view...
-		if($fourthBG.hasClass("inview")){
+		if($fourthBG.hasClass("inview")) {
 			//call the newPos function and change the background position
 			$fourthBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 1250, 0.3)});
 			$('iframe').css('height',610);
@@ -119,22 +116,22 @@ $(document).ready(function() { //when the document is ready...
 		}
 		
 		//if the fifth section is in view...
-		if($fifthBG.hasClass("inview")){
+		if($fifthBG.hasClass("inview")) {
 			//call the newPos function and change the background position
 			$fifthBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 2850, 0.3)});
 			var newTop = (windowHeight + pos)*0.4 - windowHeight;
-			floaters.css('top', newTop); 
+			floaters.css('top', newTop);
 		}
 	}
 		
 	RepositionNav(); //Reposition the Navigation to center it in the window when the script loads
 	////repos_intro();
-	$window.resize(function(){ //if the user resizes the window...
+	$window.resize(function() { //if the user resizes the window...
 		Move(); //move the background images in relation to the movement of the scrollbar
 		RepositionNav(); //reposition the navigation list so it remains vertically central
-	});		
+	});
 	
-	$window.bind('scroll', function(){ //when the user is scrolling...
+	$window.bind('scroll', function() { //when the user is scrolling...
 		Move(); //move the background images in relation to the movement of the scrollbar
 	});
 	
