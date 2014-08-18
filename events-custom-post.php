d213fb0e82e1f320d7ce6aa603c966e7b0cbf0c2
Tom Henderson <tomhenderson@mac.com>
2014-08-18 13:43:35 +1200

    Tidy up code and add some comments.

--- events-custom-post.php
+++ events-custom-post.php
@@ -1,7 +1,6 @@
 <?php
 
-// Custom post type with custom fields for events
-
+# Define the custom post type
 function create_events_post_type() {
 	register_post_type( 'events', 
 		array(
@@ -33,7 +32,16 @@ function create_events_post_type() {
 	);
 }
 
+# Define custom fields for the custom post type
+function events_post_type_meta() {
+    // add_meta_box( $id, $title, $callback, $post_type, $context, $priority, $callback_args );
+    add_meta_box('_event_date', __('Date'),         'display_custom_field', 'events', 'side', 'low', array('field_name' => '_event_date'));
+    add_meta_box('_event_time', __('Time'),         'display_custom_field', 'events', 'side', 'low', array('field_name' => '_event_time'));
+    add_meta_box('_event_price', __('Door Charge'), 'display_custom_field', 'events', 'side', 'low', array('field_name' => '_event_price'));
+    add_meta_box('_event_url', __('Event Url'),     'display_custom_field', 'events', 'side', 'low', array('field_name' => '_event_url'));
+}
 
+# Define the columns to display in the admin list view
 function events_custom_columns($columns) {
     $columns = array(
         'cb' => '<input type="checkbox" />',
@@ -46,6 +54,7 @@ function events_custom_columns($columns) {
     return $columns;
 }
 
+# Function to fetch the values for custom folumns in the admin list view
 function events_manage_custom_columns($column, $post_id) {
     global $post;
 
@@ -60,6 +69,7 @@ function events_manage_custom_columns($column, $post_id) {
     }
 }
 
+# Define custom fields for the custom post type
 function events_post_type_meta() {
     // add_meta_box( $id, $title, $callback, $post_type, $context, $priority, $callback_args );
     add_meta_box('_event_date', __('Date'),         'display_custom_field', 'events', 'side', 'low', array('field_name' => '_event_date'));
@@ -68,6 +78,7 @@ function events_post_type_meta() {
     add_meta_box('_event_url', __('Event Url'),     'display_custom_field', 'events', 'side', 'low', array('field_name' => '_event_url'));
 }
 
+# Function to output form fields for custom fields
 function display_custom_field($post, $args) {
     global $post;
     $current_value = get_post_meta($post->ID, $args['args']['field_name'], true);
@@ -75,6 +86,7 @@ function display_custom_field($post, $args) {
     echo '<input type="text" name="', $args['args']['field_name'], '" value="', $current_value, '" class="widefat" />'; 
 }
 
+# Save function for custom fields
 function save_events_post_type_meta($post_id , $post) { 
     if ( !wp_verify_nonce($_POST['banner-buttonmeta_noncename'], plugin_basename(__FILE__)) ) {
         return $post->ID; 
@@ -86,7 +98,6 @@ function save_events_post_type_meta($post_id , $post) {
 
     $meta_field_names = array('_event_url', '_event_price', '_event_date', '_event_time');
 
-    # error_log(print_r($_POST));
     foreach ( $meta_field_names as $meta_field ) {
     	$post_data[$meta_field] = $_POST[$meta_field]; 
         foreach ( $post_data as $key => $value ) {
@@ -95,6 +106,7 @@ function save_events_post_type_meta($post_id , $post) {
     }
 }
 
+# Utility function to handle CRUD functions called when saving each custom field
 function process_meta_field($post, $key, $value) {
     if ( $post->post_type == 'revision' ) {
         return;
@@ -113,6 +125,7 @@ function process_meta_field($post, $key, $value) {
     }
 }
 
+# Hooks to implement custom post
 add_action('init', 'create_events_post_type');
 add_action('admin_init', 'events_post_type_meta');
 add_filter('manage_events_posts_columns', 'events_custom_columns');
